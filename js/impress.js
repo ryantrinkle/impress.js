6d5b194ac23922e68b415cff0c642b5ad81f51d0
Ryan Trinkle <ryan.trinkle@gmail.com>
2015-03-19 13:44:14 -0400

    Don't respond to hotkeys in textboxes

--- js/impress.js
+++ js/impress.js
@@ -365,7 +365,9 @@
             
             css(body, {
                 height: "100%",
-                overflow: "hidden"
+                width: "100%",
+                overflow: "hidden",
+                position: "fixed"
             });
             
             var rootStyles = {
@@ -681,8 +683,11 @@
         
         // Prevent default keydown action when one of supported key is pressed.
         document.addEventListener("keydown", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
-                event.preventDefault();
+            var tagName = ( event.target || event.srcElement ).tagName;
+            if ( tagName !== 'INPUT' && tagName !== 'SELECT' && tagName !== 'TEXTAREA' ) {
+                if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+                    event.preventDefault();
+                }
             }
         }, false);
         
@@ -702,23 +707,26 @@
         //   as another way to moving to next step... And yes, I know that for the sake of
         //   consistency I should add [shift+tab] as opposite action...
         document.addEventListener("keyup", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
-                switch( event.keyCode ) {
-                    case 33: // pg up
-                    case 37: // left
-                    case 38: // up
-                             api.prev();
-                             break;
-                    case 9:  // tab
-                    case 32: // space
-                    case 34: // pg down
-                    case 39: // right
-                    case 40: // down
-                             api.next();
-                             break;
+            var tagName = ( event.target || event.srcElement ).tagName;
+            if ( tagName !== 'INPUT' && tagName !== 'SELECT' && tagName !== 'TEXTAREA' ) {
+                if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+                    switch( event.keyCode ) {
+                        case 33: // pg up
+                        case 37: // left
+                        case 38: // up
+                                 api.prev();
+                                 break;
+                        case 9:  // tab
+                        case 32: // space
+                        case 34: // pg down
+                        case 39: // right
+                        case 40: // down
+                                 api.next();
+                                 break;
+                    }
+                    
+                    event.preventDefault();
                 }
-                
-                event.preventDefault();
             }
         }, false);
         
